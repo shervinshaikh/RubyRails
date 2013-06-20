a2d7d9c2813a914f37229cdc3844c909400622e8
Shervin Shaikh <shervin.shaikh@appfolio.com>
2013-06-19 18:19:42 -0700

    Forms

--- rails/flix/app/controllers/movies_controller.rb
+++ rails/flix/app/controllers/movies_controller.rb
@@ -8,4 +8,16 @@ class MoviesController < ApplicationController
 		@movie = Movie.find(params[:id])
 	end
 
+	def edit
+		@movie = Movie.find(params[:id])
+	end
+
+	def update
+		@movie = Movie.find(params[:id])
+		movie_params = params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
+		# .permit! if all are updatable without listing them
+		@movie.update(movie_params)
+		redirect_to @movie
+	end
+
 end
--- rails/flix/config/routes.rb
+++ rails/flix/config/routes.rb
@@ -2,6 +2,8 @@ Flix::Application.routes.draw do
   root "movies#index"
   get "movies" => "movies#index"
   get "movies/:id" => "movies#show", as: "movie"
+  get "movies/:id/edit" => "movies#edit", as: "edit_movie"
+  patch "movies/:id" => "movies#update"
 
 
   # The priority is based upon order of creation: first created -> highest priority.
